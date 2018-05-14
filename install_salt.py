aae1dd2f0982659f0380db6638baf7ea16f0968f
Lee <leescoding@gmail.com>
2018-05-13 23:03:39 -0600

    Updated to support autosign grains

--- install_salt.py
+++ install_salt.py
@@ -24,18 +24,6 @@ from vboxmanager.utils.sshutil import OpenSSH
 from utils.salt.keys import SaltMasterKey
 
 
-# Creates the UUID to be used to accept the Minions key on the Master
-NAMESPACE_STR = 'linuxminion'
-MINION_UUID = str(uuid.uuid3(uuid.NAMESPACE_DNS, NAMESPACE_STR))
-
-salt_dir = '/etc/salt'
-minion_file = os.path.join(salt_dir, 'minion_id')
-basedir = os.path.abspath(os.path.dirname(__file__))
-salt_master_ip = "%salt_master%"
-hostname = "%reverse.dep.VirtualBox_BuildVirtualMachine_LinuxMinion.hostname%"
-ipaddr = "%reverse.dep.VirtualBox_BuildVirtualMachine_LinuxMinion.ipaddress%"
-username = "%reverse.dep.VirtualBox_BuildVirtualMachine_LinuxMinion.username%"
-password = "%reverse.dep.VirtualBox_BuildVirtualMachine_LinuxMinion.password%"
 
 def run_command(command, host, port, timeout):
 	ssh = OpenSSH(hostname=ipaddr, username=username, password=password, timeout=timeout)
@@ -50,20 +38,34 @@ if __name__ == '__main__':
 	# (will automate this process at some point but for now, this works)
 	timeout = 900
 
+	# Creates the UUID to be used to accept the Minions key on the Master
+	NAMESPACE_STR = 'linuxminion'
+	MINION_UUID = str(uuid.uuid3(uuid.NAMESPACE_DNS, NAMESPACE_STR))
+
+	salt_dir = '/etc/salt'
+	minion_file = os.path.join(salt_dir, 'minion_id')
+	basedir = os.path.abspath(os.path.dirname(__file__))
+	salt_master_ip = "%salt_master%"
+	hostname = "%reverse.dep.VirtualBox_BuildVirtualMachine_LinuxMinion.hostname%"
+	ipaddr = "%reverse.dep.VirtualBox_BuildVirtualMachine_LinuxMinion.ipaddress%"
+	username = "%reverse.dep.VirtualBox_BuildVirtualMachine_LinuxMinion.username%"
+	password = "%reverse.dep.VirtualBox_BuildVirtualMachine_LinuxMinion.password%"
+
 	# Install curl, cd into source dir, curl salt bootstrap script and install
 	# Salt minion with Master node specified in build parameter
 	salt_master = SaltMasterKey('public')
 	master_key = salt_master.get_key()
 	commands = [
-		'sudo apt-get install curl -y',
-		'cd /usr/local/src',
-		'sudo curl -L https://bootstrap.saltstack.com -o install_salt.sh',
-		'sudo sh install_salt.sh -A {}'.format(salt_master_ip),
-		'sudo sed -Ei \'s/#master\: salt/master\: {}/\' /etc/salt/minion'.format(salt_master_ip),
-		'sudo sed -Ei "s/^#master_finger: .*/master_finger: \'{pub_key}\'/" /etc/salt/minion'.format(pub_key=master_key),
-		'sudo echo {} > {}'.format(hostname, minion_file),
-		'sudo /etc/init.d/salt-minion restart',
-		'sudo sed -Ei "s/^#.*(\- uuid)/\1\n  {uuid}/"'.format(MINION_UUID)
+		r'sudo apt-get install curl -y',
+		r'sudo curl -L https://bootstrap.saltstack.com -o install_salt.sh',
+		r'sudo sh install_salt.sh -A {}'.format(salt_master_ip),
+		r'sudo sed -Ei \'s/#master\: salt/master\: {}/\' /etc/salt/minion'.format(salt_master_ip),
+		r'sudo sed -Ei "s/^#master_finger: .*/master_finger: \'{pub_key}\'/" /etc/salt/minion'.format(pub_key=master_key),
+		r'sudo rm -f /etc/salt/minion_id',
+		r'sudo sed -i "s/^#grains\:/grains\:\n  uuid\: \'{uuid}\'/" /etc/salt/minion'.format(uuid=MINION_UUID),
+	    r'sudo sed -i "/uuid\: \'{uuid}\'/a autosign_grains\:\n  - uuid" /etc/salt/minion'.format(uuid=MINION_UUID),
+	    r'sudo sed -i "s/^#id\:/id\: {}/" /etc/salt/minion'.format(hostname),
+	    r'sudo /etc/init.d/salt-minion restart'
 	]
 
 	for command in commands:
